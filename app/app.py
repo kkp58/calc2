7249004c42ccdd5ece849ec28b0043c5631b27a7
Krishna Patel <kkp58@njit.edu>
2021-12-12 12:07:46 -0500

    Changes to get other pages

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 """A simple flask web app"""
 from flask import Flask
-from app.controllers.index_controller import IndexController
 from app.controllers.calculator_controller import CalculatorController
 from app.controllers.table_controller import TableController
+from flask import render_template
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/", methods=['GET'])
 def index_get():
-    return IndexController.get()
+    return CalculatorController.get()
 
 @app.route("/calculator", methods=['GET'])
 def calculator_get():
@@ -22,3 +22,19 @@ def calculator_post():
 @app.route("/table", methods = ['GET'])
 def table_get_post():
     return TableController.get()
+
+@app.route("/history", methods = ['GET'])
+def history_page():
+    return render_template("history.html")
+
+@app.route("/AAATesting", methods = ['GET'])
+def aaa_page():
+    return render_template("AAATesting.html")
+
+@app.route("/design", methods = ['GET'])
+def design_page():
+    return render_template("design.html")
+
+@app.route("/pylintAndOthers", methods = ['GET'])
+def pylint():
+    return render_template("pylintAndOthers.html")
--- app/controllers/table_controller.py
+++ app/controllers/table_controller.py
@@ -1,10 +1,16 @@
+import os
+
 from app.controllers.controller import ControllerBase
 from flask import render_template
 import pandas as pd
 
+
 class TableController(ControllerBase):
     @staticmethod
     def get():
-        df = pd.read_csv("result.csv", header=None)
-        values = df.values.tolist()
-        return render_template('table.html', values=values)
+        if os.stat("result.csv").st_size == 0:
+            return render_template('empty.html')
+        else:
+            df = pd.read_csv("result.csv", header=None)
+            values = df.values.tolist()
+            return render_template('table.html', values=values)
